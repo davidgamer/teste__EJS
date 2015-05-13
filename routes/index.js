eb8ee3a70f59d90781c6d0679458ae3ee212e50d
David Silva <davidhenriquesi@live.com>
2015-05-13 16:04:00 -0300

    ;

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Teste Ejs Rocks...' });
+});
+
+router.get('/who/:name?', function(req,res){
+  
+  var name = req.params.name;
+  res.send(name + ' Esteve aki');
+  
+  
+  
 });
 
 module.exports = router;
+
