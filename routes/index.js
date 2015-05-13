0f44595e974c4dbdc4472f1319e6dac597972fdc
David Silva <davidhenriquesi@live.com>
2015-05-13 16:49:24 -0300

    man this is crazy

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Teste Ejs Rocks...' });
-});
-
-router.get('/who/:name?', function(req,res){
+  res.render('index', { title: 'Teste Ejs Rocks...',
+  users: ['David','Nayara','Adriane']
   
-  var name = req.params.name;
-  res.send(name + ' Esteve aki');
-  
-  
-  
+  });
 });
 
 module.exports = router;
